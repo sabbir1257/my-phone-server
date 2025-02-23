0bf6d7fe04a76c8561d6871973ac581add40da89
sabbir1257 <masabbir1257@gmail.com>
2025-02-23 23:58:22 +0600

    Create get all data api and single phone api

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get('/phones', (req, res) => {
 res.send(phones)
 })
 
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('i need data for id: ' ,id)
+    const phone = phones.find( phone => phone.id === id) || {};
+    res.send(phone)
+})
+
 app.listen(port, () => {
     console.log(`My phone server is running on port: ${port}`)
 })
+
+
